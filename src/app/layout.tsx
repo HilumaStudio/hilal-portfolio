869324169f6fe84ccd17f3852335adb1c841cc72
Hiluma Studio <info@hilumastudio.com>
2025-08-23 12:46:43 +0700

    install vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,7 +35,8 @@ export default function RootLayout({
         className= {`${inter.variable} antialiased`}
       >
         {children}
-        <SpeedInsights/>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
